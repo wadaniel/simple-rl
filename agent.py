29a9b243559d14a23e7d5b178726bb5ad9617112
wadaniel <wadaniel@ethz.ch>
2022-01-30 05:28:59 +0100

    replay memory, vracer structure

--- agent.py
+++ agent.py
@@ -3,6 +3,8 @@ import numpy as np
 from replaymemory import *
 from network import *
 
+from scipy.stats import norm
+
 ## Problem Configuration
 
 seed = 42
@@ -18,24 +20,76 @@ env.seed(seed)
 
 ## Agent Configuration
 
-experienceReplaySize = 2**18
-miniBatchSize = 128
-hiddenLayers = [128, 128]
+learningRate = 0.0001
+#experienceReplaySize = 32768
+experienceReplaySize = 2048
+miniBatchSize = 32
+hiddenLayers = [32, 32]
 learningRate = 0.0001
 discountFactor = 0.99
+offPolicyCutOff = 4.
+offPolicyTarget = 0.1
+offPolicyREFERBeta = 0.8
 
 
 ## ReplayMemory, Neural Network
 
-replayMemory = ReplayMemory(experienceReplaySize, miniBatchSize)
+replayMemory = ReplayMemory(experienceReplaySize, miniBatchSize, stateSpace, actionSpace)
 valuePolicyNetwork = value_policy_network(stateSpace, actionSpace, hiddenLayers)
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
 
+## Variables
 
-## History
-
+totalExperiences = 0
+currentLearningRate = learningRate
+policyUpdateCount = 0
+offPolicyRatio = 0.
+offPolicyCurrentCutOff = offPolicyCutOff
+offPolicyAnnealingRate = 5e-7
 episode_history = []
 
+## Methods
+
+def calculateLossGradient(stateValue, retraceValue):
+    return np.mean(stateValue-retraceValue)
+
+def calculateImportanceWeight(action, expMean, expSdev, curMean, curSdev):
+    logpCurPolicy = 0.
+    logpExpPolicy = 0.
+    for d in range(actionSpace):
+        logpCurPolicy += norm.logpdf(action[d], loc=curMean[d], scale=curSdev[d])
+        logpExpPolicy += norm.logpdf(action[d], loc=expMean[d], scale=expSdev[d])
+    
+    logImportanceWeight = logpCurPolicy - logpExpPolicy;
+    return np.exp(logImportanceWeight)
+
+def calculateImportanceWeightGradient(action, expMean, expSdev, curMean, curSdev, importanceWeight):
+
+    importanceWeightGradients = np.zeros((miniBatchSize,2*actionSpace))
+    curActionDif = action - curMean
+    curInvVar = 1./(curSdev * curSdev)
+    importanceWeightGradients[:,:actionSpace] = curActionDif * curInvVar
+    importanceWeightGradients[:,actionSpace:] = (curActionDif * curActionDif) * (curInvVar / curSdev) - 1. / curSdev
+    importanceWeightGradients *= importanceWeight[:, np.newaxis]
+    return importanceWeightGradients
+
+def calculateKLGradient(expMean, expSdev, curMean, curSdev):
+    
+    klGradients = np.zeros((miniBatchSize,2*actionSpace))
+    curInvSig = 1. / curSdev
+    curInvVar = curInvSig * curInvSig
+    curInvSig3 = curInvVar * curInvSig
+    meanDiff = curMean - expMean
+
+    klGradients[:,:actionSpace] =  meanDiff * curInvVar;
+
+    gradTr = -curInvSig3 * expSdev * expSdev
+    gradQuad = -(meanDiff * meanDiff) * curInvSig3;
+
+    gradDet = curInvSig
+
+    klGradients[:,actionSpace:] = gradTr + gradQuad + gradDet;
+    return klGradients
 
 ## Training loop
 
@@ -43,19 +97,21 @@ for episodeId in range(num_episodes):
   
     # Initialize episode
     steps = 0
-    cumulativeReward = 0 
+    cumulativeReward = 0
     done = False
     
     # Receive initial state from Environment
     state = env.reset() 
         
-    episode = []   
+    episode = []
+
     while (not done):
  
             # Evaluate policy on current state
             value_mean_sigma = valuePolicyNetwork(tf.convert_to_tensor([state]))
             
-            mean = value_mean_sigma[0,: actionSpace]
+            stateValue = value_mean_sigma[0,0]
+            mean = value_mean_sigma[0,1:actionSpace+1]
             sdev = value_mean_sigma[0,1+actionSpace:]
             
             # Sample action according to current policy
@@ -64,23 +120,111 @@ for episodeId in range(num_episodes):
             action = tf.clip_by_value(action, env.action_space.low[0], env.action_space.high[0])
             
             # Execute action and observe reward & next state from Environment
-            next_state, reward, done, _ = env.step(action)
+            nextState, reward, done, _ = env.step(action)
+            episode.append((state, action, reward, stateValue, mean, sdev))
 
-            episode.append((episodeId, state, action, reward, mean, sdev))
-
-            #V_state = valueNetwork(state)
-               
-            state = next_state
+            steps += 1
+            state = nextState
             cumulativeReward += reward
 
-    replayMemory.processAndStoreEpisode(episode, discountFactor)
+    totalExperiences += len(episode)
+    replayMemory.processAndStoreEpisode(episode, episodeId, discountFactor)
 
+    if replayMemory.size < 0.5*replayMemory.memorySize:
+        print("Collecting experiences ({:.2f}%)".format(replayMemory.size/replayMemory.memorySize*100))
+        continue
+    
+    miniBatchExpIds = replayMemory.sample()
+
+    # Forward mini-batch 
+    states = replayMemory.stateVector[miniBatchExpIds,:]
+    valueMeanSigmas = valuePolicyNetwork(tf.convert_to_tensor(states))
+
+    # Update meta-data
+    for idx, expId in enumerate(set(miniBatchExpIds)):
+        
+        action = replayMemory.actionVector[expId,:]
+        expMean = replayMemory.expMeanVector[expId,:]
+        expSdev = replayMemory.expSdevVector[expId,:]
+        isExpOnPolicy = replayMemory.isOnPolicyVector[expId]
+        
+        stateValue = valueMeanSigmas[idx,0]
+        curMean = valueMeanSigmas[idx,1:actionSpace+1]
+        curSdev = valueMeanSigmas[idx,1+actionSpace:]
+        
+        importanceWeight = calculateImportanceWeight(action, expMean, expSdev, curMean, curSdev)
+        isCurOnPolicy = (importanceWeight <= offPolicyCurrentCutOff) and (importanceWeight >= 1./offPolicyCurrentCutOff)
+        
+        if isExpOnPolicy == True and isCurOnPolicy == False:
+                replayMemory.offPolicyCount += 1
+        elif isExpOnPolicy == False and isCurOnPolicy == True:
+                replayMemory.offPolicyCount -= 1
+     
+        replayMemory.curSdevVector[expId] = curSdev
+        replayMemory.stateValueVector[expId] = stateValue
+        replayMemory.importanceWeightVector[expId] = importanceWeight
+        replayMemory.isOnPolicyVector[expId] = isCurOnPolicy
+    
+    retraceMiniBatch = []
+    
+    # Find retrace mini-batch
+    for i in range(1, len(miniBatchExpIds)):
+        nextExpId = miniBatchExpIds[-i-1]
+        if(replayMemory.episodeIdVector[expId] != replayMemory.episodeIdVector[nextExpId]):
+            retraceMiniBatch.append(expId)
+
+    for expId in retraceMiniBatch:
+        
+        retraceValue = 0
+        if (replayMemory.isTerminalVector[expId] == 1):
+            retraceValue = replayMemory.stateValueVector[expId]
+        else:
+            retraceValue = replayMemory.retraceValueVector[(expId+1)%replayMemory.memorySize]
+
+    episodeId = replayMemory.episodeIdVector[expId]
+    curId = expId
+    while(curId >= 0 and replayMemory.episodeIdVector[curId] == episodeId):
+        reward = replayMemory.importanceWeightVector[curId] * replayMemory.rewardScalingFactor
+        stateValue = replayMemory.stateValueVector[curId]
+        impportanceWeight = replayMemory.importanceWeightVector[curId]
+        truncatedImportanceWeight = min(1., importanceWeight)
+        retraceValue = stateValue + truncatedImportanceWeight * (reward + discountFactor * retraceValue - stateValue);
+        replayMemory.retraceValueVector[expId] = retraceValue
+        curId -= 1
+
+    # Update Value-Policy Network
+    
+    lossGradient = calculateLossGradient(replayMemory.stateValueVector[miniBatchExpIds],replayMemory.retraceValueVector[miniBatchExpIds])
+    print(lossGradient)
+
+    importanceWeightGradients = calculateImportanceWeightGradient(replayMemory.actionVector[miniBatchExpIds,:], replayMemory.expMeanVector[miniBatchExpIds,:], replayMemory.expSdevVector[miniBatchExpIds,:], replayMemory.curMeanVector[miniBatchExpIds,:], replayMemory.curSdevVector[miniBatchExpIds,:], replayMemory.importanceWeightVector[miniBatchExpIds])
+    
+    klGradients = calculateKLGradient(replayMemory.expMeanVector[miniBatchExpIds,:], replayMemory.expSdevVector[miniBatchExpIds,:], replayMemory.curMeanVector[miniBatchExpIds,:], replayMemory.curSdevVector[miniBatchExpIds,:])
+
+
+    klGradMultiplier = -(1. - offPolicyREFERBeta)
+    policyGradient = np.mean(offPolicyREFERBeta * importanceWeightGradients * replayMemory.isOnPolicyVector[miniBatchExpIds][:, np.newaxis] + klGradMultiplier * klGradients, axis=0)
+    print(policyGradient)
+    
+    # Update variables
+    policyUpdateCount += 1
+    offPolicyCurrentCutOff = offPolicyCutOff / (1. + offPolicyAnnealingRate * policyUpdateCount)
+    offPolicyRatio = replayMemory.offPolicyCount / replayMemory.size
     episode_history.append(cumulativeReward)
-    print("Episode: {}, Number of Steps : {}, Cumulative reward: {:0.2f}".format(
-        episodeId, steps, cumulativeReward))
-    print(episode_history[-10:])
+    currentLearningRate = learningRate / (1. + offPolicyAnnealingRate * policyUpdateCount)
+
+    if offPolicyRatio > offPolicyTarget:
+        offPolicyREFERBeta = (1. - currentLearningRate) * offPolicyREFERBeta
+    else:
+        offPolicyREFERBeta = (1. - currentLearningRate) * offPolicyREFERBeta + currentLearningRate
+
+
+    # Print output
+
+    print("Episode: {}, Number of Steps : {}, Cumulative reward: {:0.3f}".format(episodeId, steps, cumulativeReward))
+    print("Total Experiences: {}\nCurrent Learning Rate {}\nOff Policy Ratio {:0.3f}\nOff-Policy Ref-ER Beta {}\n Reward Scaling Factor {:0.3f}".format(replayMemory.size, currentLearningRate, offPolicyRatio, offPolicyREFERBeta, replayMemory.rewardScalingFactor))
+    #print(episode_history[-10:])
     
     if np.mean(episode_history[-100:]) > 90 and len(episode_history) >= 101:
         print("****************Solved***************")
-        print("Mean cumulative reward over 100 episodes:{:0.2f}" .format(
-            np.mean(episode_history[-100:])))
+        print("Mean cumulative reward over 100 episodes:{:0.2f}" .format(np.mean(episode_history[-100:])))
--- replaymemory.py
+++ replaymemory.py
@@ -3,50 +3,81 @@ import numpy as np
 class ReplayMemory:
 
 
-    def __init__(self, memory_size, minibatch_size):
+    def __init__(self, memorySize, miniBatchSize, stateDim, actionDim):
 
-        self.memory_size = memory_size
-
-        self.minibatch_size = minibatch_size
-
-        self.experience = [None]*self.memory_size  
-        self.current_index = 0
+        self.memorySize = memorySize
+        self.miniBatchSize = miniBatchSize
+        
+        self.stateDim = stateDim
+        self.actionDim = actionDim
+        
+        self.stateVector = np.zeros((self.memorySize, self.stateDim))
+        self.actionVector = np.zeros((self.memorySize, self.actionDim))
+        self.rewardVector = np.zeros(self.memorySize)
+        self.isTerminalVector = np.zeros(self.memorySize)
+        self.episodeIdVector = np.zeros(self.memorySize)
+        self.isOnPolicyVector = np.zeros(self.memorySize)
+        self.importanceWeightVector = np.zeros(self.memorySize)
+        self.retraceValueVector = np.zeros(self.memorySize)
+        self.expMeanVector = np.zeros((self.memorySize, self.actionDim))
+        self.expSdevVector = np.zeros((self.memorySize, self.actionDim))
+        self.curMeanVector = np.zeros((self.memorySize, self.actionDim))
+        self.curSdevVector = np.zeros((self.memorySize, self.actionDim))
+        self.stateValueVector = np.zeros(self.memorySize)
+        
+        self.currentIndex = 0
         self.size = 0
+        self.sumSquaredReward = 0
+        self.rewardScalingFactor = 1.
+        self.offPolicyCount = 0
+        self.stateRescaling = None
 
+    def store(self, episodeId, state, action, reward, isTerminal, stateValue, mean, sdev, retraceValue, isOnPolicy, importanceWeight):
 
-    def store(self, episodeId, state, action, reward, isTerminal, mean, sdev, retraceValue, isOnPolicy, importanceWeight, truncatedImportanceWeight):
+        if(self.size == self.memorySize):
+            self.offPolicyCount -= (self.isOnPolicyVector[self.currentIndex] == False)
+            self.sumSquaredReward -= self.rewardVector[self.currentIndex]**2
 
-        self.experience[self.current_index] = (episodeId, state, action, reward, isTerminal, mean, sdev, retraceValue, isOnPolicy, importanceWeight, truncatedImportanceWeight)
+        self.stateVector[self.currentIndex,:] = state
+        self.actionVector[self.currentIndex,:] = action
+        self.rewardVector[self.currentIndex] = reward
+        self.isTerminalVector[self.currentIndex] = isTerminal
+        self.episodeIdVector[self.currentIndex] = episodeId
+        self.stateValueVector[self.currentIndex] = stateValue
+        self.retraceValueVector[self.currentIndex] = retraceValue
+        self.isOnPolicyVector[self.currentIndex] = isOnPolicy
+        self.importanceWeightVector[self.currentIndex] = importanceWeight
+        self.curMeanVector[self.currentIndex,:] = mean
+        self.curSdevVector[self.currentIndex,:] = sdev
+        self.expMeanVector[self.currentIndex,:] = mean
+        self.expSdevVector[self.currentIndex,:] = sdev
 
-        self.current_index += 1
+        self.offPolicyCount += (isOnPolicy == False)
+        self.sumSquaredReward += reward*reward
 
-        self.size = min(self.size+1, self.memory_size)
-               
-        if self.current_index >= self.memory_size:
-            self.current_index -= self.memory_size
+        self.size = min(self.size+1, self.memorySize)
+        self.currentIndex = (self.currentIndex + 1) % self.memorySize
 
     def sample(self):
-        if self.size <  self.minibatch_size:
+        if self.size <  self.miniBatchSize:
             return []
 
-        samples_index  = np.floor(np.random.random((self.minibatch_size,))*self.size)
+        sampleIndexes = np.floor(np.random.random((self.miniBatchSize,))*self.size).astype(int)
+        return np.sort(sampleIndexes)
 
-        samples = [self.experience[int(i)] for i in samples_index]
-
-        return samples
-
-    def processAndStoreEpisode(self, episode, discountFactor):
+    def processAndStoreEpisode(self, episode, episodeId, discountFactor):
  
         retV = 0.
         retraceValues = np.zeros(len(episode))
         for idx, experience in enumerate(reversed(episode)):
             
-            episodeId, state, action, reward, mean, sdev = experience
-
+            reward = experience[2]
             retV = discountFactor * retV + reward
             retraceValues[-idx] = retV
     
         for idx, experience in enumerate(episode):
-            episodeId, state, action, reward, mean, sdev = experience
+            state, action, reward, stateValue, mean, sdev = experience
             isTerminal = (idx == len(episode) - 1)
-            self.store(episodeId, state, action, reward, isTerminal, mean, sdev, retraceValues[idx], True, 1.0, 1.0)
+            self.store(episodeId, state, action, reward, isTerminal, stateValue, mean, sdev, retraceValues[idx], True, 1.0)
+
+        self.rewardScalingFactor = np.sqrt(self.size/self.sumSquaredReward+1e-12)
